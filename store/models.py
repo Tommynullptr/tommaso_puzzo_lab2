92f3c4e7e0a3968ad257fad051a8a1d46c7d5783
tom <tommaso.puzzo@stud.unifi.it>
2023-06-06 13:42:15 +0200

    Added models, urls, views and fixture file for later.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
 
 # Create your models here.
+
+# TODO: CustomUserManager? [slide-5 pag 12]
+
+
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True, )
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+
+class CustomUser(AbstractBaseUser, models.Model):
+    username = models.CharField(max_length=255, primary_key=True, unique=True)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    USERNAME_FIELD = 'username'
+    # should own a cart
+    # cart = models.ForeignKey('Cart', on_delete=models.CASCADE, null=True)
+
+
+class Cart(models.Model):
+    listofproducts = models.ManyToManyField(Product)
+    totalprice = models.FloatField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+
+
+class Order(models.Model):
+    listofproducts = models.ManyToManyField(Product)
+    totalprice = models.FloatField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+
+# TODO: alternative to manytomanyfield: add a field to the product model that is a list of users who have it in their cart and use a queryset
+
--- store/views.py
+++ store/views.py
@@ -1,4 +1,77 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+from django.views import View
+from django.views.generic import FormView, ListView, DetailView
+
+from store.models import Cart, Order, Product
+#from store.forms import UserFormRegister, UserFormLogin
+
 
 # Create your views here.
+
+def index(request):
+
+    return HttpResponse("Hello, world. You're at the store index.")
+
+
+class UserView(View): #TODO: make this view use the forms [check chatgpt (template loader configuration) + slide-4 pag 23,26 + slide-5 pag 22]
+
+    template_name = 'user.html'
+
+    def get(self, request):
+
+        return HttpResponse("Hello, world. You're at the store user.")
+
+
+class CartView(ListView):
+
+    model = Cart
+    template_name = 'cart.html'
+
+    def get_queryset(self):
+
+        return Cart.objects.all().filter(user=self.request.user)
+
+
+    def get(self, request: HttpRequest, *args: str, **kwargs: str) -> HttpResponse:
+
+        if not request.user.is_authenticated:
+            return redirect("home")
+
+        return HttpResponse("Hello, world. You're at your cart.") #return super().get(request, *args, **kwargs)
+
+
+class OrderView(ListView):
+
+    model = Order
+    template_name = 'order.html'
+
+    def get_queryset(self):
+
+        return Order.objects.all().filter(user=self.request.user)
+
+
+    def get(self, request: HttpRequest, *args: str, **kwargs: str) -> HttpResponse:
+
+        if not request.user.is_authenticated:
+            return redirect("home")
+
+        return HttpResponse("Hello, world. You're at your order.") #return super().get(request, *args, **kwargs)
+
+
+class ProductView(DetailView):
+
+    model = Product
+    template_name = 'product.html'
+
+    def get(self, request):
+
+        return HttpResponse("Hello, world. You're at the store product.")
+
+    #def get(self, request, *args, **kwargs):
+        # Call the parent class's get() method to trigger the template rendering process
+        #return super().get(request, *args, **kwargs)
+
+
+
+#TODO: create cart and order CreateView? [slide-4 pag 5]
