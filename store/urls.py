df67488d424a77941800e51b587e1601cce88c7b
tom <tommaso.puzzo@stud.unifi.it>
2023-06-06 18:16:03 +0200

    Created new views and urls. Created new templates.

--- store/urls.py
+++ store/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path
-from .views import index
+#from django.urls import path
+#from .views import index
 
 
-urlpatterns = [
-    path("", index, name='home'),
-    path("user/", index, name='user'),
-    path("cart/<str:username>/", index, name='cart'),
-    path("order/<str:username>/", index, name='order'),
-    path("product/<int:id>", index, name='product'),
-]
+#urlpatterns = [
+ #   path("", index, name='home'),
+  #  path("user/", index, name='user'),
+   # path("cart/<str:username>/", index, name='cart'),
+  #  path("order/<str:username>/", index, name='order'),
+  #  path("product/<int:id>", index, name='product'),
+#]
 
 #TODO: change to correct urls (user, cart, order, product)
 
@@ -16,3 +16,20 @@ urlpatterns = [
 
 #TODO: index is the view function to call, change to correct view functions for each url (user, cart, order, product)
 #each url, when matched with the request, should call the correct view function dinamically (usernames and product ids)
+
+##############################################################################################
+
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('cart/', views.cart, name='cart'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('order-confirmation/', views.order_confirmation, name='order_confirmation'),
+]
--- store/views.py
+++ store/views.py
@@ -1,72 +1,72 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpRequest
-from django.views import View
-from django.views.generic import FormView, ListView, DetailView
+#from django.shortcuts import render, redirect
+#from django.http import HttpResponse, HttpRequest
+#from django.views import View
+#from django.views.generic import FormView, ListView, DetailView
 
-from store.models import Cart, Order, Product
-from store.forms import UserFormRegister, UserFormLogin
+#from store.models import Cart, Order, Product
+#from store.forms import UserFormRegister, UserFormLogin
 
 
 # Create your views here.
 
-def index(request):
+#def index(request):
 
-    return HttpResponse("Hello, world. You're at the store index.")
-
-
-class UserView(View): #TODO: make this view use the forms [check chatgpt (template loader configuration) + slide-4 pag 23,26 + slide-5 pag 22]
-
-    template_name = 'user.html'
-
-    def get(self, request):
-
-        return HttpResponse("Hello, world. You're at the store user.")
+ #   return HttpResponse("Hello, world. You're at the store index.")
 
 
-class CartView(ListView):
+#class UserView(View): #TODO: make this view use the forms [check chatgpt (template loader configuration) + slide-4 pag 23,26 + slide-5 pag 22]
 
-    model = Cart
-    template_name = 'cart.html'
+ #   template_name = 'user.html'
 
-    def get_queryset(self):
+ #   def get(self, request):
 
-        return Cart.objects.all().filter(user=self.request.user)
-
-
-    def get(self, request: HttpRequest, *args: str, **kwargs: str) -> HttpResponse:
-
-        if not request.user.is_authenticated:
-            return redirect("home")
-
-        return HttpResponse("Hello, world. You're at your cart.") #return super().get(request, *args, **kwargs)
+ #       return HttpResponse("Hello, world. You're at the store user.")
 
 
-class OrderView(ListView):
+#class CartView(ListView):
 
-    model = Order
-    template_name = 'order.html'
+ #   model = Cart
+ #   template_name = 'cart.html'
 
-    def get_queryset(self):
+  #  def get_queryset(self):
 
-        return Order.objects.all().filter(user=self.request.user)
-
-
-    def get(self, request: HttpRequest, *args: str, **kwargs: str) -> HttpResponse:
-
-        if not request.user.is_authenticated:
-            return redirect("home")
-
-        return HttpResponse("Hello, world. You're at your order.") #return super().get(request, *args, **kwargs)
+   #     return Cart.objects.all().filter(user=self.request.user)
 
 
-class ProductView(DetailView):
+   # def get(self, request: HttpRequest, *args: str, **kwargs: str) -> HttpResponse:
 
-    model = Product
-    template_name = 'product.html'
+    #    if not request.user.is_authenticated:
+     #       return redirect("home")
 
-    def get(self, request):
+      #  return HttpResponse("Hello, world. You're at your cart.") #return super().get(request, *args, **kwargs)
 
-        return HttpResponse("Hello, world. You're at the store product.")
+
+#class OrderView(ListView):
+
+ #   model = Order
+  #  template_name = 'order.html'
+
+   # def get_queryset(self):
+
+    #    return Order.objects.all().filter(user=self.request.user)
+
+
+    #def get(self, request: HttpRequest, *args: str, **kwargs: str) -> HttpResponse:
+
+     #   if not request.user.is_authenticated:
+      #      return redirect("home")
+
+       # return HttpResponse("Hello, world. You're at your order.") #return super().get(request, *args, **kwargs)
+
+
+#class ProductView(DetailView):
+
+ #   model = Product
+  #  template_name = 'product.html'
+
+   # def get(self, request):
+
+    #    return HttpResponse("Hello, world. You're at the store product.")
 
     #def get(self, request, *args, **kwargs):
         # Call the parent class's get() method to trigger the template rendering process
@@ -75,3 +75,59 @@ class ProductView(DetailView):
 
 
 #TODO: create cart and order CreateView? [slide-4 pag 5]
+
+##############################################################################################################
+
+
+from django.shortcuts import render, redirect
+from .models import Product, Cart, Order
+
+# Product Listing View
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+# Product Detail View
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
+
+# Add Item to Cart View
+def add_to_cart(request, product_id):
+    if request.method == 'POST':
+        product = Product.objects.get(id=product_id)
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        cart.listofproducts.add(product)
+        cart.totalprice += product.price
+        cart.save()
+        return redirect('cart')
+
+# Cart View
+def cart(request):
+    cart = Cart.objects.get(user=request.user)
+    return render(request, 'cart.html', {'cart': cart})
+
+# Remove Item from Cart View
+def remove_from_cart(request, product_id):
+    if request.method == 'POST':
+        product = Product.objects.get(id=product_id)
+        cart = Cart.objects.get(user=request.user)
+        cart.listofproducts.remove(product)
+        cart.totalprice -= product.price
+        cart.save()
+        return redirect('cart')
+
+# Checkout View
+def checkout(request):
+    if request.method == 'POST':
+        cart = Cart.objects.get(user=request.user)
+        order = Order.objects.create(user=request.user, totalprice=cart.totalprice)
+        order.listofproducts.set(cart.listofproducts.all())
+        cart.listofproducts.clear()
+        cart.totalprice = 0
+        cart.save()
+        return redirect('order_confirmation')
+
+# Order Confirmation View
+def order_confirmation(request):
+    return render(request, 'order_confirmation.html')
