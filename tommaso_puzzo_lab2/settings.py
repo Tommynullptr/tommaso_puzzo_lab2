63a1f33d33ac1fcfca3fdc4f6d72e2bdb6651027
tom <tommaso.puzzo@stud.unifi.it>
2023-06-12 11:34:55 +0200

    Updated settings.

--- tommaso_puzzo_lab2/settings.py
+++ tommaso_puzzo_lab2/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'tommaso_puzzo_lab2')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
