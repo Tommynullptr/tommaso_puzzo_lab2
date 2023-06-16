f5568ee308979a7fd5030ce078abcd0849c065f8
tom <tommaso.puzzo@stud.unifi.it>
2023-06-16 16:38:09 +0200

    Debug=False

--- tommaso_puzzo_lab2/settings.py
+++ tommaso_puzzo_lab2/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-17741j^is2bya!nr^12ygha_$62m@zi6(kl+@zayws_o4w(8zv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
